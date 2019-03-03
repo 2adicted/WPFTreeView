34a4220d7f078a048cf82bdeb3a282b653faadf0
2adicted <dido.nenov@gmail.com>
2019-03-03 21:38:31 +0000

    First commit

---
+++ Directory/ViewModels/NaturalComparer.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WpfTreeView
+{
+    public class NaturalStringComparer : IComparer<string>
+    {
+        public int Compare(string left, string right)
+        {
+            int max = new[] { left, right }
+                .SelectMany(x => Regex.Matches(x, @"\d+").Cast<Match>().Select(y => (int?)y.Value.Length))
+                .Max() ?? 0;
+
+            var leftPadded = Regex.Replace(left, @"\d+", m => m.Value.PadLeft(max, '0'));
+            var rightPadded = Regex.Replace(right, @"\d+", m => m.Value.PadLeft(max, '0'));
+
+            return string.Compare(leftPadded, rightPadded);
+        }
+    }
+}
